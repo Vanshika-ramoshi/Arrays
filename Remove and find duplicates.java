0c32b256a44583e6a8b6f5c76e0ef78ebd1816e0
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-08-25 21:53:48 +0530

    Update and rename Remove duplicates.java to Remove and find duplicates.java

--- Remove and find duplicates.java
+++ Remove and find duplicates.java
@@ -3,7 +3,7 @@ package arrays;
 public class Frequency {
 
 	public static void main(String[] args) {
-//merge two 1D ARRAY
+//remove duplicatess
     int[] num = {1,2,2,2,3,4,4,5,6,6,5 };
 		int[] result = new int[num.length];
 		int count = 0;
@@ -23,6 +23,18 @@ public class Frequency {
 		}
 		for (int k = 0; k < count; k++) {
 			System.out.println(result[k]);
+		}
+
+		//find duplicates
+		int[] num = {1,2,2,2,3,4,4,5,6,6,5 };
+		
+		for (int i = 0; i < num.length; i++) {
+			
+			for (int j = 0; j <num.length; j++) {
+				if (num[i] == num[j]) {
+					System.out.println("duplicates = "+num[i]);
+				}
+			}
 		}
 	}
 
