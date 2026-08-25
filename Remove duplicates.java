9dd1efbea0db9a37faa394174033a3570e0eec23
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-08-25 21:44:29 +0530

    Fix duplicate detection logic in Remove duplicates.java

---
+++ Remove duplicates.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class Frequency {
+
+	public static void main(String[] args) {
+//merge two 1D ARRAY
+    int[] num = {1,2,2,2,3,4,4,5,6,6,5 };
+		int[] result = new int[num.length];
+		int count = 0;
+
+		for (int i = 0; i < num.length; i++) {
+			boolean dublicate = false;
+			for (int j = 0; j < count; j++) {
+				if (num[i] == result[j]) {
+					dublicate = true;
+					break;
+				}
+			}
+			if (!dublicate) {
+				result[count] = num[i];
+				count++;
+			}
+		}
+		for (int k = 0; k < count; k++) {
+			System.out.println(result[k]);
+		}
+	}
+
+}
